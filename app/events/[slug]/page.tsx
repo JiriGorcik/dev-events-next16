da38ab262a1983490058972cd345b4cedabf6d7b
Jiří <jirigorcik@seznam.cz>
2026-01-11 21:22:43 +0100

    Finalize the App

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -4,6 +4,7 @@ import BookEvent from '@/components/BookEvent';
 import { IEvent } from '@/database';
 import { getSimilarEventsBySlug } from '@/lib/actions/event.actions';
 import EventCard from '@/components/EventCard';
+import { cacheLife } from 'next/cache';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -48,6 +49,8 @@ const EventDetailsPage = async ({
 }: {
   params: Promise<{ slug: string }>;
 }) => {
+  'use cache';
+  cacheLife('hours');
   const { slug } = await params;
 
   let event;
@@ -156,7 +159,7 @@ const EventDetailsPage = async ({
               <p className="text-sm">Be the first to book your spot!</p>
             )}
 
-            <BookEvent />
+            <BookEvent eventId={event._id} slug={event.slug} />
           </div>
         </aside>
       </div>
